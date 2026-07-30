3a62b58e309798359e290e5c64f62bbc26ddeabc
Swapnil Adyalkar <swapnilswapnil20@gmail.com>
2026-07-31 02:49:56 +0530

    module

--- infra-app/dynamodb.tf
+++ infra-app/dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "${var.env}-infra-app-table"
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "var.hash_key"
+  hash_key       = var.hash_key
 
 
   attribute {
