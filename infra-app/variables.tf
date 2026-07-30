633104f22a867366657b2ace850d5825770f379e
Swapnil Adyalkar <swapnilswapnil20@gmail.com>
2026-07-31 02:07:40 +0530

    module

--- infra-app/variables.tf
+++ infra-app/variables.tf
@@ -9,21 +9,21 @@ variable "bucket_name" {
 }
 
 variable "instance_count" {
-    description "this is the number of ec2 instance for my infra"
+    description = "this is the number of ec2 instance for my infra"
     type = number
 }
 
 variable "instance_type" {
-    description "this is the instance type for for my infra"
+    description = "this is the instance type for for my infra"
     type = string
 }
 
 variable "ec2_ami_id" {
-    description "this is the ami id for my infra"
+    description ="this is the ami id for my infra"
     type = string
 }
 
 variable "hash_key" {
-    description "this is hash key for my dynamodb infra"
+    description = "this is hash key for my dynamodb infra"
     type = string
 }
