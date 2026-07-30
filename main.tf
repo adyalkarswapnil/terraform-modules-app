82769741526511ffc22f99d685233154eef17f5a
Swapnil Adyalkar <swapnilswapnil20@gmail.com>
2026-07-31 04:20:58 +0530

    module

--- main.tf
+++ main.tf
@@ -9,3 +9,25 @@ module "dev-infra" {
   ec2_ami_id     = "ami-00d2dbb426772b03a" # Example AMI ID for Amazon Linux 2
   hash_key       = "studentId"
 }
+
+module "dev-infra" {
+  source = "./infra-app"
+
+  env            = "prd"
+  bucket_name    = "infra-app-bucket"
+  instance_count = 2
+  instance_type  = "t3.small"
+  ec2_ami_id     = "ami-00d2dbb426772b03a" # Example AMI ID for Amazon Linux 2
+  hash_key       = "studentId"
+}
+
+module "dev-infra" {
+  source = "./infra-app"
+
+  env            = "stag"
+  bucket_name    = "infra-app-bucket"
+  instance_count = 3
+  instance_type  = "c7i-flex.large"
+  ec2_ami_id     = "ami-00d2dbb426772b03a" # Example AMI ID for Amazon Linux 2
+  hash_key       = "studentId"
+}
