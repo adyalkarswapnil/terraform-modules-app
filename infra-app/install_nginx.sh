8a7c38df9ed9e41baf7c95cd265bb83087a22f79
Swapnil Adyalkar <swapnilswapnil20@gmail.com>
2026-07-31 02:34:21 +0530

    module

--- infra-app/install_nginx.sh
+++ infra-app/install_nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 sudo yum update -y
-sudo yum install nginx -y
+sudo amazon-linux-extras install nginx1 -y   # only needed on Amazon Linux 2
 sudo systemctl start nginx
 sudo systemctl enable nginx
-echo "<h1>Terraform Welcome to Nginx Server</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Terraform Welcome to Nginx Server</h1>" | sudo tee /usr/share/nginx/html/index.html
