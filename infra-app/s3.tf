5a098c3fb36860de17263ad107d64f95a6e92490
Swapnil Adyalkar <swapnilswapnil20@gmail.com>
2026-08-03 02:03:07 +0530

    module

--- infra-app/s3.tf
+++ infra-app/s3.tf
@@ -1,9 +1,10 @@
-resource "aws_s3_bucket" "remote_s3" {
-  bucket = "${var.env}-${var.bucket_name}"
+data "aws_caller_identity" "current" {}
 
+resource "aws_s3_bucket" "remote_s3" {
+  bucket = "${var.env}-${var.bucket_name}-${data.aws_caller_identity.current.account_id}"
 
   tags = {
-    Name        = "${var.env}-${var.bucket_name}"
+    Name        = "${var.env}-${var.bucket_name}-${data.aws_caller_identity.current.account_id}"
     Environment = var.env
   }
 }
