9db1ddf1822fcc37f50531bf01e687b9ce3f7aff
Swapnil Adyalkar <swapnilswapnil20@gmail.com>
2026-08-06 04:10:05 +0530

    eks cluster

---
+++ terraform-eks/eks.tf
@@ -0,0 +1,49 @@
+module "eks" {
+  #import the module template
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  #cluster info(control plane)
+  cluster_name = local.name
+  cluster_endpoint_public_access  = true
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_addons = {
+    vpc_cni = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    core-dns = {
+      most_recent = true
+    }
+  }
+
+  #control plane network
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+
+  #managing nodes in the cluster
+  eks_managed_node_groups_default = {
+
+    instance_types = ["t3.micro"]
+    attach_cluster_primary_security_group = true
+  }
+
+ eks_managed_node_group {
+ tws-cluster-ng = {
+ instance_types = ["t3.micro"]
+ min_size = 2
+ max_size = 3
+ desired_size = 2
+ capacity_type = "SPOT"
+}
+}
+
+    tags = {
+    Environment = "local.evn"
+    Terraform   = "true"
+  }
+}
